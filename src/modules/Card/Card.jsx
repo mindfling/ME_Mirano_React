f850a6c17308e69dea56074edf7b3279297cfc58
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-04 00:30:05 +0300

    feat Card

--- src/modules/Card/Card.jsx
+++ src/modules/Card/Card.jsx
@@ -1,7 +1,6 @@
-// подключаем дополнительную библиотеку для удобства classNames
+import './card.scss';
 import classNames from "classnames";
 
-// принимаем пропсы с деструктуризацией
 export const Card = ({ title, img, dateDelivery, price, id, className }) => {
 
   return (
@@ -18,26 +17,3 @@ export const Card = ({ title, img, dateDelivery, price, id, className }) => {
   );
 }
 
-
-/*
-export const Card = ({ data, className }) => {
-  const {title, img, dateDelivery, price, id} = data;
-  console.log('className: ', className);
-
-
-  return (
-    <article className={className ? className + " card" : "card"} title={title}>
-      <img className="card__image" src={img}
-        alt={title}
-      />
-      <div className="card__content">
-        <h3 className="card__title">{title}</h3>
-        <div className="card__footer">
-          <p className="card__date-delivery">{dateDelivery}</p>
-          <button className="card__button">{price}&nbsp;₽</button>
-        </div>
-      </div>
-    </article>
-  );
-}
-*/
