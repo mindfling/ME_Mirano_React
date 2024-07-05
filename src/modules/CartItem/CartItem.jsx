2f472ef98c2f75fe332c0f5f5f230e6cdb33a677
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-05 14:39:49 +0300

    Update CartItem.jsx

--- src/modules/CartItem/CartItem.jsx
+++ src/modules/CartItem/CartItem.jsx
@@ -1,26 +1,18 @@
-// import React from 'react'
-//
-
-// {
-//   id: 2,
-//   img: "https://dull-rose-pawpaw.glitch.me/img/39.jpg",
-//   title: "Букет из роз Grand Avalanche (101 шт)",
-//   dateDelivery: "сегодня в 14:00",
-//   price: "14800",
-// },
+/* data = item
+{
+  id: 2,
+  img: "https://dull-rose-pawpaw.glitch.me/img/39.jpg",
+  title: "Букет из роз Grand Avalanche (101 шт)",
+  dateDelivery: "сегодня в 14:00",
+  price: "14800",
+},
+*/
 
 export const CartItem = ({ data }) => {
-  
   return (
     <li className="cart__item">
-      <img
-        className="cart__img"
-        src={data.img}
-        alt={data.title}
-      />
-      <h4 className="cart__item-title">
-        {data.title}
-      </h4>
+      <img className="cart__img" src={data.img} alt={data.title} />
+      <h4 className="cart__item-title">{data.title}</h4>
       <div className="cart__counter">
         <button className="cart__counter-btn">-</button>
         <input
