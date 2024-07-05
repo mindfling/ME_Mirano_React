4cc56ec6f307c3c5ef0b55080b7c87901314c2a5
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-05 19:51:45 +0300

    feat Order render ternar operator

--- src/modules/Order/Order.jsx
+++ src/modules/Order/Order.jsx
@@ -3,137 +3,128 @@ import "./order.scss";
 // Order с условием
 export const Order = () => {
   // todo
-  const isOrder = false;
+  const isOrder = true;
   const isOpen = false;
 
-  if (!isOpen) {
-    return null;
-  }
+  if (!isOpen) return null;
 
   const orderAmount = 2100;
   const orderHashNumber = "0x971f365a-caa1-4cdb-9446-bad2eff047e1";
 
-  if (isOrder) {
-    // форма заказа
-    return (
-      <>
-        <div className="order">
-          <div className="order__wrapper">
-            <h2 className="order__title">Оформить заказ</h2>
-            <form className="order__form" id="order">
-              <fieldset className="order__fieldset">
-                <legend className="order__legend">Данные заказчика</legend>
-                <div className="order__input-group">
-                  <input
-                    className="order__input"
-                    type="text"
-                    name="name-buyer"
-                    placeholder="Имя"
-                  />
-                  <input
-                    className="order__input"
-                    type="text"
-                    name="phone-buyer"
-                    placeholder="Телефон"
-                  />
-                </div>
-              </fieldset>
-              <fieldset className="order__fieldset">
-                <legend className="order__legend">Данные получателя</legend>
-                <div className="order__input-group">
-                  <input
-                    className="order__input"
-                    type="text"
-                    name="name-recipient"
-                    placeholder="Имя"
-                  />
-                  <input
-                    className="order__input"
-                    type="text"
-                    name="phone-recipient"
-                    placeholder="Телефон"
-                  />
-                </div>
-              </fieldset>
-              <fieldset className="order__fieldset">
-                <legend className="order__legend">Адрес</legend>
-                <div className="order__input-group">
-                  <input
-                    className="order__input"
-                    type="text"
-                    name="street"
-                    placeholder="Улица"
-                  />
-                  <input
-                    className="order__input order__input_min"
-                    type="text"
-                    name="house"
-                    placeholder="Дом"
-                  />
-                  <input
-                    className="order__input order__input_min"
-                    type="text"
-                    name="apartment"
-                    placeholder="Квартира"
-                  />
-                </div>
-              </fieldset>
-              <fieldset className="order__fieldset">
-                <div className="order__payment">
-                  <label className="order__label-radio">
+  return (
+    <>
+      <div className="order">
+        <div className="order__wrapper">
+          {isOrder ? (
+            <>
+              <h2 className="order__title">Заказ оформлен!</h2>
+              <p className="order__id">Ваш номер заказа: {orderHashNumber}</p>
+            </>
+          ) : (
+            <>
+              <h2 className="order__title">Оформить заказ</h2>
+              <form className="order__form" id="order">
+                <fieldset className="order__fieldset">
+                  <legend className="order__legend">Данные заказчика</legend>
+                  <div className="order__input-group">
                     <input
-                      className="order__radio"
-                      type="radio"
-                      name="payment-online"
-                      value="true"
-                      defaultChecked="true"
+                      className="order__input"
+                      type="text"
+                      name="name-buyer"
+                      placeholder="Имя"
                     />
-                    Оплата онлайн
-                  </label>
-                </div>
-                <div className="order__delivery">
-                  <label htmlFor="delivery">Доставка 01.07</label>
-                  <input type="hidden" name="delivery-date" value="01.07" />
-                  <div className="order__select-wrapper">
-                    <select
-                      className="order__select"
-                      name="delivery-time"
-                      id="delivery"
-                    >
-                      <option value="9-12">с 9:00 до 12:00</option>
-                      <option value="12-15">с 12:00 до 15:00</option>
-                      <option value="15-18">с 15:00 до 18:00</option>
-                      <option value="18-21">с 18:00 до 21:00</option>
-                    </select>
+                    <input
+                      className="order__input"
+                      type="text"
+                      name="phone-buyer"
+                      placeholder="Телефон"
+                    />
                   </div>
-                </div>
-              </fieldset>
-            </form>
-            <div className="order__footer">
-              <p className="order__total">{orderAmount}&nbsp;₽</p>
-              <button className="order__button" type="submit" form="order">
-                Заказать
-              </button>
-            </div>
-          </div>
-          <button className="order__close" type="button">
-            ×
-          </button>
+                </fieldset>
+                <fieldset className="order__fieldset">
+                  <legend className="order__legend">Данные получателя</legend>
+                  <div className="order__input-group">
+                    <input
+                      className="order__input"
+                      type="text"
+                      name="name-recipient"
+                      placeholder="Имя"
+                    />
+                    <input
+                      className="order__input"
+                      type="text"
+                      name="phone-recipient"
+                      placeholder="Телефон"
+                    />
+                  </div>
+                </fieldset>
+                <fieldset className="order__fieldset">
+                  <legend className="order__legend">Адрес</legend>
+                  <div className="order__input-group">
+                    <input
+                      className="order__input"
+                      type="text"
+                      name="street"
+                      placeholder="Улица"
+                    />
+                    <input
+                      className="order__input order__input_min"
+                      type="text"
+                      name="house"
+                      placeholder="Дом"
+                    />
+                    <input
+                      className="order__input order__input_min"
+                      type="text"
+                      name="apartment"
+                      placeholder="Квартира"
+                    />
+                  </div>
+                </fieldset>
+                <fieldset className="order__fieldset">
+                  <div className="order__payment">
+                    <label className="order__label-radio">
+                      <input
+                        className="order__radio"
+                        type="radio"
+                        name="payment-online"
+                        value="true"
+                        defaultChecked="true"
+                      />
+                      Оплата онлайн
+                    </label>
+                  </div>
+                  <div className="order__delivery">
+                    <label htmlFor="delivery">Доставка 01.07</label>
+                    <input type="hidden" name="delivery-date" value="01.07" />
+                    <div className="order__select-wrapper">
+                      <select
+                        className="order__select"
+                        name="delivery-time"
+                        id="delivery"
+                      >
+                        <option value="9-12">с 9:00 до 12:00</option>
+                        <option value="12-15">с 12:00 до 15:00</option>
+                        <option value="15-18">с 15:00 до 18:00</option>
+                        <option value="18-21">с 18:00 до 21:00</option>
+                      </select>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+              <div className="order__footer">
+                <p className="order__total">{orderAmount}&nbsp;₽</p>
+                <button className="order__button" type="submit" form="order">
+                  Заказать
+                </button>
+              </div>
+            </>
+          )}
         </div>
-      </>
-    );
-  } else {
-
-    // заказ оформлен
-    return (
-      <>
-        <div className="order">
-          <div className="order__wrapper">
-            <h2 className="order__title">Заказ оформлен!</h2>
-            <p className="order__id">Ваш номер заказа: {orderHashNumber}</p>
-          </div>
-        </div>
-      </>
-    );
-  }
+        <button className="order__close" type="button">
+          ×
+        </button>
+      </div>
+    </>
+  );
 };
