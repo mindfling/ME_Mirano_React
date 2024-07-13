51c551b6821ddefc06dba993eec6cbc5cf471b59
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-13 16:47:21 +0300

    hack onChangeHandler useState

--- src/modules/FilterRadio/FilterRadio.jsx
+++ src/modules/FilterRadio/FilterRadio.jsx
@@ -1,50 +1,59 @@
-import React from "react";
+import { useState } from "react";
 
 export const FilterRadio = () => {
-  // filter radio menu buttons
+  const [currentType, setCurrentType] = useState("postcards");
 
+  // filter radio menu buttons
   const filterTypes = [
     {
-      id: 'flower',
-      name: 'type',
-      value: 'bouquets',
-      text: 'Цветы',
+      id: "flower",
+      name: "type",
+      value: "bouquets",
+      text: "Цветы",
     },
     {
-      name: 'type',
-      value: 'toys',
-      id: 'toys',
-      text: 'Игрушки',
+      name: "type",
+      value: "toys",
+      id: "toys",
+      text: "Игрушки",
     },
     {
-      className: 'filter__radio',
-      type: 'radio',
-      name: 'type',
-      value: 'postcards',
-      id: 'postcard',
-      text: 'Открытки',
+      name: "type",
+      value: "postcards",
+      id: "postcard",
+      text: "Открытки",
     },
-  ]
+  ];
 
-  const handleTypeChange = (e) => {
-    console.log(e.target);
-  }
   
+  // const handleTypeChange = (e) => {
+  //   console.log(e.target, e.target.value);
+  // };
 
-  return (<>
-    {filterTypes.map((item) => (<>
+  // todo with Redux
+  const onChangeHandler = (e) => {
+    setCurrentType(() => e.target.value);
+    console.log("Radio changed", `e.target.value`, e.target.value);
+  };
+
+  return filterTypes.map((item, index) => (
+    <div className="radioitem" key={index}>
       <input
         className="filter__radio"
         type="radio"
         name="type"
         value={item.value}
         id={item.id}
-        checked={item.value === 'bouquets'}
-        onClick={handleTypeChange}
+        checked={item.value === currentType}
+        // onClick={handleTypeChange}
+        onChange={onChangeHandler}
       />
-      <label className={"filter__label filter__label_" + item.id} htmlFor={item.id}>
+      <label
+        className={"filter__label filter__label_" + item.id}
+        htmlFor={item.id}
+      >
         {item.text}
       </label>
-    </>))}
-    </>);
+    </div>
+  ));
 };
