d75f29f33aa208f92e8411480b48249832b4b1f5
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-19 01:27:19 +0300

    feat use Maks mirano-api

--- src/const.js
+++ src/const.js
@@ -2,5 +2,5 @@ export const LOCAL_URL = 'http://localhost:3000';
 export const MAX_URL = 'https://mirano-api.onrender.com';
 export const OTHER_URL = 'https://mirano-api-4txw.onrender.com'
 
-// export const API_URL = MAX_URL; 
-export const API_URL = LOCAL_URL; 
+export const API_URL = MAX_URL; 
+// export const API_URL = LOCAL_URL; 
