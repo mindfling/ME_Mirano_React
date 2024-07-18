b7bc89d296c2a339af3490e2448480907bddfd14
Nepochtov Dmitry <dimendroider@gmail.com>
2024-07-18 16:01:38 +0300

    меняем заголовок сайта через useState в App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Filter } from "./modules/Filter/Filter";
 import { Footer } from "./modules/Footer/Footer";
 import { Goods } from "./modules/Goods/Goods";
@@ -5,16 +6,34 @@ import { Header } from "./modules/Header/Header";
 import { Hero } from "./modules/Hero/Hero";
 import { Order } from "./modules/Order/Order";
 import { Subscribe } from "./modules/Subscribe/Subscribe";
+import { useDispatch } from "react-redux";
+import { registerCart } from "./redux/cartSlice";
+
+const scrollToFilter = () => {
+  console.log("scrollToFilter in Header");
+};
 
 function App() {
+  const dispatch = useDispatch();
+  const [titleGoods, setTitleGoods] = useState(""); // изменяем заголовок при нажатии на FilterRadio
+
+  // at once call registerCart
+  useEffect(() => {
+    const initCart = async () => {
+      console.log("initCart: ");
+      await dispatch(registerCart());
+    };
+    initCart();
+  });
+
   return (
     <>
-      <Header />
+      <Header scrollToFilter={scrollToFilter} />
 
       <main>
         <Hero />
-        <Filter />
-        <Goods />
+        <Filter setTitleGoods={setTitleGoods} />
+        <Goods title={titleGoods} />
         <Subscribe />
       </main>
 
